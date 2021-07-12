6c0c9b266c484ece9f8ffc406da385def3b58542
Baiadil <adikmain@gmail.com>
2021-07-12 18:55:14 +0600

    deleted django files

--- d2synergy/synergy/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SynergyConfig(AppConfig):
-    name = 'synergy'
--- d2synergy/synergy/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
-from .models import *
-
-
-class HomeView(TemplateView):
-    template_name = 'synergy/index.html'
